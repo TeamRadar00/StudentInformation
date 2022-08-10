ab4c5ebe44998bbb11bac8829d2a5b6048b6d8e6
choihuk <love72huk@naver.com>
2022-08-10 18:44:30 +0900

    update

--- src/main/java/com/studentinformation/controller/LectureController.java
+++ src/main/java/com/studentinformation/controller/LectureController.java
@@ -1,6 +1,5 @@
 package com.studentinformation.controller;
 
-import com.studentinformation.domain.Member;
 import com.studentinformation.domain.Week;
 import com.studentinformation.domain.form.LectureForm;
 import com.studentinformation.domain.form.MemberForm;
@@ -11,9 +10,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalTime;
 import java.time.OffsetTime;
@@ -37,16 +34,23 @@ public class LectureController {
         return Map.of("professor", "교수님 성함으로 검색", "lecture", "강의이름으로 검색");
     }
 
-    private void addTestData(Model model, String name) {
+    private void addTestLectureList(Model model, String name) {
         MemberForm professor = MemberForm.createMemberForm(memberService.findByMemberNum("123"));
-        model.addAttribute(name, List.of(new LectureForm("c언어", professor,
+        model.addAttribute(name, List.of(new LectureForm(1L,"c언어", professor,
                 "2022/2", Week.MONDAY, OffsetTime.of(LocalTime.now(), ZoneOffset.ofHours(2)), 20)));
     }
-
+    private void addTestLecture(Model model, String name) {
+        MemberForm professor = MemberForm.createMemberForm(memberService.findByMemberNum("123"));
+        model.addAttribute(name, new LectureForm(1L,"c언어", professor,
+                "2022/2", Week.MONDAY, OffsetTime.of(LocalTime.now(), ZoneOffset.ofHours(2)), 20));
+    }
 
     @GetMapping("/lectures/my")
     public String goMyLecture(Model model) {
-        addTestData(model, "myLectures");
+        //List<Lecture> list = lectureService.findByStudentName("choi")로 자신의 lectureList를 받고
+        //list.stream().map(LectureForm::createLectureForm).collect(Collectors.toList())로 변환 어떰?
+
+        addTestLectureList(model, "lectureList");
         return "lectures/myLecture";
     }
 
@@ -55,9 +59,26 @@ public class LectureController {
 
     @PostMapping("/lectures/opened")
     public String searchLecture(@ModelAttribute SearchLectureForm form , Model model) {
-        addTestData(model, "lectureList");
+        //lectureService.search(form)로 폼을 넘길까 아님 여기서 폼을 까내서 안에 로직에 따라 findByProfessorName이나
+        //findByLectureName을 호출할까?
+
+        addTestLectureList(model, "lectureList");
         log.info("SearchLectureForm = {}", form);
         return "lectures/openedLecture";
+    }
+
+    @GetMapping("/lectures")
+    public String goCRUDLecture(Model model) {
+        //세션 넣으면 파라미터에 @Login Member professor 넣고 해당 교수에 따라 개설된 강의 보여주기
+        addTestLectureList(model,"lectureList");
+        return "lectures/CRUDLecture";
+    }
+
+    @GetMapping("/lectures/{lectureId}/edit")
+    public String getEditLecture(@PathVariable long lectureId, Model model) {
+//        model.addAttribute("editLecture", LectureForm.createLectureForm(lectureService.findByLectureId(lectureId)));
+        addTestLecture(model, "editLecture");
+        return "lectures/editLecture";
     }
 
     @GetMapping("/lecture/applicationPage")
@@ -65,10 +86,5 @@ public class LectureController {
         return "lecture/applicationPage";
     }
 
-
-    @GetMapping("/lecture/CRUDLecture")
-    public String goCRUDLecture() {
-        return "lecture/CRUDLecture";
-    }
 
 }
