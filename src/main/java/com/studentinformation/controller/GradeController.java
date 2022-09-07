2600115326e0f1cb510cbfcee59297bf0e42685c
tlarbals824 <simkm0824@naver.com>
2022-09-07 13:47:58 +0900

    Grade controller 수정

--- src/main/java/com/studentinformation/controller/GradeController.java
+++ src/main/java/com/studentinformation/controller/GradeController.java
@@ -1,19 +1,19 @@
 package com.studentinformation.controller;
 
-import com.studentinformation.domain.Grade;
-import com.studentinformation.domain.Lecture;
-import com.studentinformation.domain.Member;
-import com.studentinformation.domain.MemberState;
-import com.studentinformation.domain.form.GoMyGradeForm;
-import com.studentinformation.domain.form.MemberForm;
-import com.studentinformation.domain.form.SubmitObjectionForm;
-import com.studentinformation.domain.form.TotalGradeForm;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.module.SimpleModule;
+import com.studentinformation.domain.*;
+import com.studentinformation.domain.form.*;
 import com.studentinformation.service.GradeService;
+import com.studentinformation.service.LectureService;
 import com.studentinformation.service.MemberService;
 import com.studentinformation.web.session.SessionConst;
 import com.sun.net.httpserver.HttpServer;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.catalina.SessionIdGenerator;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -23,8 +23,8 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 import java.net.http.HttpRequest;
 import java.security.PrivateKey;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+import java.util.stream.Collectors;
 
 @Slf4j
 @Controller
@@ -33,7 +33,7 @@ public class GradeController {
 
     private final GradeService gradeService;
     private final MemberService memberService;
-
+    private final LectureService lectureService;
 
     /**
      * model.addAttribute 여러번 사용하지말고 form으로 변환해서 한번에 보내기
@@ -67,7 +67,32 @@ public class GradeController {
     }
 
     @GetMapping("/grade/giveGrade")
-    public String goGiveGrade() {
+    public String goGiveGrade(Model model,HttpServletRequest request,
+                              @RequestParam(value = "lectureId",required = false) Long lectureId) {
+        HttpSession session = request.getSession();
+        Member professor = memberService.getMemberFromSession(session);
+        if(lectureId != null){
+            Lecture lecture = lectureService.findByLectureId(lectureId);
+            List<Member> student = lecture.getGrades().stream()
+                            .map(Grade::getStudent)
+                            .collect(Collectors.toList());
+            session.setAttribute(SessionConst.Grade_List,lecture.getGrades());
+            model.addAttribute("studentList",student);
+        }
+        model.addAttribute("lectureList",professor.getProfessorLectures());
+        return "grade/giveGrade";
+    }
+
+    @PostMapping("/grade/giveGrade")
+    public String submitGiveGrade(HttpServletRequest request,
+                                  @RequestParam("gradeScore") List<Score> scoreList) {
+        HttpSession session = request.getSession();
+        List<Grade> gradeList = (List<Grade>) session.getAttribute(SessionConst.Grade_List);
+        session.removeAttribute(SessionConst.Grade_List);
+        for(int i = 0 ; i < gradeList.size(); i++){
+            Grade grade = gradeList.get(i);
+            gradeService.editGradeOfScore(grade.getId(),scoreList.get(i));
+        }
         return "grade/giveGrade";
     }
 
